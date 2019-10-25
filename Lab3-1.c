35da76db5273616f229ed9ae5eb3094866c2aabd
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2019-10-25 09:04:20 +0300

    Add files via upload

---
+++ Lab3-1.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double a, b;
+int i;
+
+int main(int acgc, char const *argv[])
+{
+    a=1
+    b=3
+
+    for(i=1; i<=500; i++)
+    {
+        if(i%2==1)
+        {
+            cur-=
+        }
+    }
+}
