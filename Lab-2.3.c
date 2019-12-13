83740630c8520810ba8b2b7060d7f12faad1553e
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2019-12-13 09:07:03 +0300

    Update Lab-2.3.c

--- Lab-2.3.c
+++ Lab-2.3.c
@@ -6,7 +6,7 @@ int main(int argc, char const *argv[])
     printf("1=зима, 2=весна, 3=лето, 4=осень\n");
     printf("Введите пору года-");//
     scanf("%d", &n);
-    if(n==1) printf("Декабрь, январь, февраль");
+    if(n==1) printf("Декабрь, январь, февраль");//
         else if(n==2) printf("Март, апрель, май");
             else if(n==3) printf("Июнь, июль, август");
                 else if(n==4) printf("Сентябрь, октябрь, ноябрь");
