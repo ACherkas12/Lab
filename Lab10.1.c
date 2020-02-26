8bdfab342f3b0dcb0ee8466813d6b9c6f17fc953
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2020-02-26 12:35:26 +0300

    Update Lab10.1.c

--- Lab10.1.c
+++ Lab10.1.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
-float matrix[3][4]={{0.42, -1.13, 7.05, 6.15},{1.14, -2.15, 5.11, -4.16},{-0.71, 0.81, -0.02, -0.17}};
+float matrix[3][4];
+float matrix_def[3][4]={{0.42, -1.13, 7.05, 6.15},{1.14, -2.15, 5.11, -4.16},{-0.71, 0.81, -0.02, -0.17}};
 
-void ShowMatrix(void){
-	int i,j;
-		for(i=0; i<3; i++){
-			for(j=0; j<3; j++)
-				printf("%+3.3f*x%d",matrix[i][j],j+1);
-        		printf("=%3.3f\n",matrix[i][3]);
-		}
-}
+void all_matrix(void);
+void ShowMatrix(void);
+
 int main(){
 system("chcp 1251>nul");
 	float tmp, xx[4];
 	short int i, j, k;
+	int choice;
+	printf("Выберите:\n0 - индивидуальное задание;\n1 - любая система из 3 неизвестных.\n? ");
+	scanf("%d", &choice);
+	if (choice)
+		all_matrix();
+	else
+		memcpy(matrix, matrix_def, size_of(matrix));
  
 ShowMatrix();
   for (i=0; i<3; i++)
@@ -36,10 +40,31 @@ ShowMatrix();
          xx[i] = matrix[i][3];
          for (j=i+1;j<3;j++) xx[i]-=matrix[i][j]*xx[j];
      }
-printf("\nMetod Gaussa:\n");
+printf("\nКорни уравнения по методу Гаусса:\n");
   for (i=0; i<3; i++)
     printf("x%d=%3.3f\n", i+1, xx[i]);
  system("pause>nul");
  return 0;
 }
+ 
+void ShowMatrix(void)
+{
+	int i,j;
+		for(i=0; i<3; i++){
+			for(j=0; j<3; j++)
+				printf("%+3.3f*x%d",matrix[i][j],j+1);
+        		printf("=%3.3f\n",matrix[i][3]);
+		}
+}
 
+void all_matrix(void){
+	int i, j;
+	for(i=0; i<3; i++){
+		printf("Введите числа для строки %d:\n", i+1);
+		for(j=0; j<4; j++){
+			printf("[%d][%d]: ", i, j);
+			scanf("%f", &matrix[i][j]);
+		}
+	}
+}
+				
