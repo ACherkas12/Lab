1f1d89714359e8f6a0b8fdbef9dc77ff7b5f8406
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2020-03-18 13:06:53 +0300

    Add files via upload

--- Lab12.1.c
+++ Lab12.1.c
@@ -1,38 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <time.h>
+#include <stdbool.h>
 
-int MakeHashFunc(int key, int i){
-  return key*(i+25)+13;
-}
+typedef struct info_t{
+    int value;
+    bool use;
+} TInfo;
 
-int main(){
-  system("chcp 1251>nul");
-  int n;
-    printf("Введите размер массива:");
-    scanf("%d", n);
-    srand(time(NULL));
-    int a[n], i, b[n], max=0;
-      for(i=0;i<n;i++){
-        a[i]=rand()%100;
-        printf("key(%d)", a[i]);
-        b[i]=MakeHashFunc(a[i], i);
-      }
-  printf("\n\n Сгенерированная таблица: ");
-  for(i=0;i<n;i++){
-    printf("HashFunc(%d)=%d ",a[i], b[i]);
-  }
-  printf("\n\nMax key value: ");
-  for(i=0,i<n;i++){
-    if(a[i]>max){
-      max=a[i];
-    }
-  }
-  printf("key:%d; value:%d", a[i], b[i]);
-  system("pause>nul");
-  return 0;
-}
-        
+TInfo* GetTable(size_t, size) {
     
+}
